0f30af3f93f57502827420260784e5470414828d
ih <irvin.hwang@gmail.com>
2012-02-05 21:21:28 -0800

    added variable lookup

--- binary/interpreter.scm
+++ binary/interpreter.scm
@@ -1,9 +1,17 @@
-(use-modules (srfi srfi-1))
+(use-modules (srfi srfi-1)
+             (srfi srfi-69)
+             (srfi srfi-34))
 
-(define (evaluate expression) 
+(define global-environment
+  (let* ((start-frame (make-hash-table)))
+    (begin
+      (hash-table-set! start-frame 'cons cons)
+      (list start-frame))))
+
+(define (evaluate expression environment) 
   (cond ((self-evaluating? expression) expression)
-        ((variable? expression) (lookup-variable-value expression))
-        ((application? expression) (semantic-apply (evaluate (operator expression)) (operands expression)))
+        ((variable? expression) (lookup-variable-value expression environment))
+        ((application? expression) (semantic-apply (evaluate (operator expression) environment) (operands expression)))
         (else 'error)))
 
 (define (self-evaluating? expression)
@@ -11,8 +19,27 @@
 
 (define variable? symbol?)
 
-
-(define (lookup-variable-value expression) 'TODO)
+;;TODO move environment-related code into a separate file
+;;;goes through each frame of the environment looking for the target variable and returns the corresponding value if found
+(define (lookup-variable-value variable environment)
+  (define variable-found? car)
+  (define first-frame car)
+  (define variable-value cdr)
+  (define enclosing-environment cdr)
+  (define empty? null?)
+;;;checks whether variable is in the frame
+  (define (scan frame)
+    (guard (exception ((eq? exception 'key-error) (cons #f 'error)))
+           (cons #t (hash-table-ref frame variable (lambda () (raise 'key-error))))))
+  
+  (if (empty? environment)
+      'error
+      (let* ((scan-result (scan (first-frame environment))) ;;TODO change this to catching an exception rather than using an error code
+             (found (variable-found? scan-result))
+             (value (variable-value scan-result)))
+        (if found
+            value
+            (lookup-variable-value variable (enclosing-environment environment))))))
 
 (define (semantic-apply procedure arguments)
   (cond ((primitive-procedure? procedure) (apply-primitive-procedure procedure arguments))
@@ -33,8 +60,6 @@
   (if (pair? object)
       (eq? (car object) tag)
       #f))
-
-
 
 (define (learn-semantics-and-apply procedure arguments) 'TODOd)
 

5d63094f82c3f817a0be50d169887609eb459618
ih <irvin.hwang@gmail.com>
2012-03-16 10:13:22 -0700

    thinking about planning

--- binary/agent.scm
+++ binary/agent.scm
@@ -2,6 +2,8 @@
          "generators.scm"
          "classifier.scm")
 
+
+(define plan '())
 ;;(cons-programs (flip 1 0 5))
 (define (cons-programs binary-string)
   (if (null? binary-string)
@@ -13,6 +15,9 @@
   (let ((input-program (cons-programs stimuli)))
     input-program))
 
-
 ;;
-(define )
+(define (select-action input)
+  (if (or (null? plan) (not (equal? input (predicted-input plan))))
+      (generate-plan input goal)
+      (next-step plan)))
+
